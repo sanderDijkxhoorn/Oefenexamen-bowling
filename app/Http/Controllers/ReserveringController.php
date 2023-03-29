04cfd51dac379da1ac46710c830c50b90ea9c153
Sander Dijkxhoorn <15117158+sanderDijkxhoorn@users.noreply.github.com>
2023-03-29 12:41:50 +0200

    Index select + date filter

--- app/Http/Controllers/ReserveringController.php
+++ app/Http/Controllers/ReserveringController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Reservering;
+use App\Models\PakketOptie;
 
 class ReserveringController extends Controller
 {
@@ -15,13 +16,39 @@ class ReserveringController extends Controller
     public function index(Request $request)
     {
         // Selecteer alle reserveringen waar de persoon_id gelijk is aan 1
-            $reserveringen = Reservering::select('reserverings.id', 'persoon.voornaam', 'persoon.tussenvoegsel', 'persoon.achternaam', 'reserverings.datum', 'reserverings.aantal_uren', 'reserverings.begin_tijd', 'reserverings.eind_tijd', 'reserverings.aantal_volwassenen', 'reserverings.aantal_kinderen')
+        // De reserveringen moet op datum aflopend zijn
+        // Vraag de voornaam, tussenvoegsel, achternaam en combineer deze tot een volledige naam
+        // Datum, AantalUren, BeginTijd, EindTijd, AantalVolwassenen, AantalKinderen uit de reservering tabel
+        // Selecteer de optiepakket naam uit de pakket_opties tabel via de foreign key pakket_optie_id in de reservering tabel (join)
+
+        // Check of de datum niet leeg is
+        if (!empty($request->date)) {
+            // Selecteer alle reserveringen vanaf de meegegeven datum
+            $reserveringen = Reservering::select('reserverings.id', 'persoon.voornaam', 'persoon.tussenvoegsel', 'persoon.achternaam', 'reserverings.datum', 'reserverings.aantal_uren', 'reserverings.begin_tijd', 'reserverings.eind_tijd', 'reserverings.aantal_volwassenen', 'reserverings.aantal_kinderen', 'pakket_opties.naam')
+                ->join('pakket_opties', 'pakket_opties.id', '=', 'reserverings.pakket_optie_id')
+                ->join('persoon', 'persoon.id', '=', 'reserverings.persoon_id')
+                ->where('persoon.id', '=', 1)
+                ->where('reserverings.datum', '>=', $request->date)
+                ->orderBy('reserverings.datum', 'desc')
+                ->get();
+
+            // Check of er wel reserveringen zijn
+            if ($reserveringen->isEmpty()) {
+                // Geen reserveringen gevonden
+                return redirect()->route('reserveringen.index')->with('error', 'Er zijn geen reserveringen gevonden voor de datum: ' . $request->date);
+            }
+        } else {
+            // Selecteer alle reserveringen voor die persoon
+            $reserveringen = Reservering::select('reserverings.id', 'persoon.voornaam', 'persoon.tussenvoegsel', 'persoon.achternaam', 'reserverings.datum', 'reserverings.aantal_uren', 'reserverings.begin_tijd', 'reserverings.eind_tijd', 'reserverings.aantal_volwassenen', 'reserverings.aantal_kinderen', 'pakket_opties.naam')
+                ->join('pakket_opties', 'pakket_opties.id', '=', 'reserverings.pakket_optie_id')
                 ->join('persoon', 'persoon.id', '=', 'reserverings.persoon_id')
                 ->where('persoon.id', '=', 1)
                 ->orderBy('reserverings.datum', 'desc')
                 ->get();
+        }
 
         // Return de view met de reserveringen
         return view('reserveringen.index', compact('reserveringen'));
     }
+
 }
